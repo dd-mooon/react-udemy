88af64f8da820a60f618ff795e2ebfcd803b88cb
dd-mooon <vjwyp0308@gmail.com>
2026-01-08 15:42:27 +0900

    feat: tabs component - setting component dynamically

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import TebButton from './TebButton/TebButton.jsx';
 import { EXAMPLES } from '../data.js';
 import Section from'./Section.jsx'
+import Tabs from './Tabs.jsx'
 
 export default function Examples (){
     
@@ -11,26 +12,33 @@ export default function Examples (){
     setSelectedTopic(selecteButton);
     console.log(selectedTopic);
   }
+
+  let tabContent = <p>Pleas select a topic.</p>
+  if(selectedTopic) {
+    tabContent = ( 
+      <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+      </div>
+    )
+  }
   
   return (
         <Section id="examples" title="Examples">
-          <menu>
+          <Tabs buttons={
+          <>
             <TebButton label="Component" isSelected = {selectedTopic === 'components'} onSelect={()=> clickSource('components')} />
             <TebButton label="JSX" isSelected = {selectedTopic === 'jsx'} onSelect={()=> clickSource('jsx')} />
             <TebButton label="Props" isSelected = {selectedTopic === 'props'} onSelect={()=> clickSource('props')} />
             <TebButton label="State" isSelected = {selectedTopic === 'state'} onSelect={()=> clickSource('state')} />
-          </menu>
-        
-          {!selectedTopic && <p>Plesa click a button.</p>}
-          {selectedTopic &&
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          }
+          </>
+          }>
+          {tabContent}
+          </Tabs>
+
         </Section>
   )
 }
